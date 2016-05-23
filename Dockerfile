268d92fb80e0a8642c217c4142540d2b3ec8d8e6
Michael Marr <marr@marr-tech.com>
2016-05-23 16:30:23 -0400

    changing UI mode and uploading/installing optional packages

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,13 @@ ONBUILD ADD license.properties /aem/license.properties
 ONBUILD ADD https://raw.githubusercontent.com/LexmarkWeb/docker-aem-author/master/postInstallHook.sh /aem/postInstallHook.sh
 ONBUILD RUN chmod +x /aem/postInstallHook.sh
 
+#uploads optional packages
+ONBUILD ADD *.zip /aem/packages/
+
 # Extracts AEM
 ONBUILD WORKDIR /aem
 ONBUILD RUN java -XX:MaxPermSize=256m -Xmx1024M -jar cq-author-4502.jar -unpack -r nosamplecontent
+
 
 # Installs AEM
 ONBUILD RUN ["/aem/aemInstaller.sh","-i","cq-author-4502.jar","-r","author","-p","4502"]
--- postInstallHook.sh
+++ postInstallHook.sh
@@ -5,6 +5,14 @@ password="admin:admin"
 
 curl $baseUrl/etc/replication/agents.author/publish/jcr:content --user $password --data-urlencode "{'transportUri': 'http://publisher:4503/bin/receive?sling:authRequestLogin=1'}"
 
-
 echo "checking agent"
 curl $baseUrl/etc/replication/agents.author/publish/jcr:content.json --user $password
+
+echo "setting UI mode to CLASSIC"
+sed -i 's/TOUCH/CLASSIC/' /aem/crx-quickstart/launchpad/config/com/day/cq/wcm/core/impl/AuthoringUIModeServiceImpl.config
+
+echo "uploading optional packages"
+for file in /aem/packages/*
+do
+  curl --user $password -F file=@"$file" -F name="$file" -F force=true -F install=true $baseUrl/crx/packmgr/service.jsp
+done
